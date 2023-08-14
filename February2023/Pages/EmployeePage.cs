febe5dfccbf79cd551c9e3d0cfc7548768979804
Ivica Cuncic <gorankogoric@yahoo.com>
2023-08-14 15:12:12 +1200

    Employee page filling up

--- February2023/Pages/EmployeePage.cs
+++ February2023/Pages/EmployeePage.cs
@@ -12,6 +12,38 @@ namespace February2023.Pages
     {
         public void CreateEmployee(IWebDriver driver)
         {
+            IWebElement createEmployee = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+            createEmployee.Click();
+            Thread.Sleep(1000);
+
+
+            IWebElement employeeName = driver.FindElement(By.Id("Name"));
+            employeeName.SendKeys("Ivica");
+
+            IWebElement employeeUserName = driver.FindElement(By.Id("Username"));
+            employeeUserName.SendKeys("Cuncic");
+
+            IWebElement contactEmployee = driver.FindElement(By.Id("ContactDisplay"));
+            contactEmployee.SendKeys("222222222");
+
+            IWebElement employeePassword = driver.FindElement(By.Id("Password"));
+            employeePassword.SendKeys("Password");
+
+            IWebElement RetypePassword = driver.FindElement(By.Id("RetypePassword"));
+            RetypePassword.SendKeys("Password");
+
+            IWebElement IsAdmin = driver.FindElement(By.Id("IsAdmin"));
+            IsAdmin.Click();
+
+            IWebElement vehicle = driver.FindElement(By.XPath("//*[@id=\"UserEditForm\"]/div/div[7]/div/span[1]/span/input"));
+            vehicle.SendKeys("Mazda");
+
+            IWebElement groups = driver.FindElement(By.XPath("//*[@id=\"UserEditForm\"]/div/div[8]/div/div/div[1]"));
+            groups.SendKeys("FFFFFFF");
+
+            IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
+            SaveButton.Click();
+
 
         }
 
--- February2023/Pages/HomePage.cs
+++ February2023/Pages/HomePage.cs
@@ -42,5 +42,6 @@ namespace February2023.Pages
 
 
         }
+
     }
 }
